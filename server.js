885ef890ec28ec534a42a0b030976bd11da5a37c
Casey Carroll <caseydean8@gmail.com>
2020-10-08 11:10:13 -0400

    Changed database to cloud.mongodb

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ require("dotenv").config();
 const app = require("./config/express-config");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://submarine:submar1ne@ds139768.mlab.com:39768/heroku_rx8cqg30";
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://submarine:submar1ne@ds139768.mlab.com:39768/heroku_rx8cqg30";
+// const MONGODB_URI =
+//   process.env.MONGODB_URI ||
+//   "mongodb+srv://dbCaseyDean:Cdc108mng@cluster0-adjxe.mongodb.net/submarineDb?retryWrites=true&w=majority";
 
 // connect to MongoDB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
+// moved database from mlag to cloud.mongodb.com. Had to create database in Cluster0 called submarine, and add collections users and subscriptions.
+
+mongoose.connect(
+  //   MONGODB_URI,
+//   process.env.MONGODB_URI ||
+    "mongodb+srv://dbCaseyDean:Cdc108mng@cluster0-adjxe.mongodb.net/submarine?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
     if (err) {
-        console.log("There is a problem with the connection" + err)
+      console.log("There is a problem with the connection" + err);
     } else {
-        console.log("Mongoose connection is good.")
+      console.log("Mongoose connection is good.");
     }
-});
+  }
+);
 
 //server is up and running
 app.listen(PORT);
