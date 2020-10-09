9f7e14c1029fe56694d46d8744679768b67723b4
Casey Carroll <caseydean8@gmail.com>
2020-10-09 11:01:26 -0400

    Moved mongoDb info to env file
    Also fixed mongoose deprecation by adding useFindAndModify: false on line 19

--- server.js
+++ server.js
@@ -2,24 +2,27 @@ require("dotenv").config();
 const app = require("./config/express-config");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://submarine:submar1ne@ds139768.mlab.com:39768/heroku_rx8cqg30";
-const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://dbCaseyDean:Cdc108mng@cluster0-adjxe.mongodb.net/submarineDb?retryWrites=true&w=majority";
 
 // connect to MongoDB
-// moved database from mlag to cloud.mongodb.com. Had to create database in Cluster0 called submarine, and add collections users and subscriptions.
+// moved database from mlab to cloud.mongodb.com. Had to create database in Cluster0 called submarine, and add collections users and subscriptions. For this to work on Heroku you have to whitelist/add IP address 0.0.0.0/0 by navigating to SECURITY/Network Access/IP Access List in the cloud.mongo dashboard. 
+//https://stackoverflow.com/questions/42159175/connecting-heroku-app-to-atlas-mongodb-cloud-service
+// Also changed config vars MONGODB_URI in Heroku settings
+
+// BYE mlab, nice knowing ya...
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://submarine:submar1ne@ds139768.mlab.com:39768/heroku_rx8cqg30";
+// const MONGODB_URI =
+//   process.env.MONGODB_URI ||
+//   "mongodb login moved to env file :)";
 
 mongoose.connect(
-    MONGODB_URI,
-//   process.env.MONGODB_URI ||
-    // "mongodb+srv://dbCaseyDean:Cdc108mng@cluster0-adjxe.mongodb.net/submarine?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true },
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
   (err) => {
     if (err) {
       console.log("There is a problem with the connection" + err);
     } else {
       console.log("Mongoose connection is good.");
+      console.log(process.env.MONGODB_URI);
     }
   }
 );
