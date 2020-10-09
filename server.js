53b8e09aa19a68de232a0d1052afd13208f676db
Casey Carroll <caseydean8@gmail.com>
2020-10-08 20:11:38 -0400

    added process.env MONGODB_UR1

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const app = require("./config/express-config");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 // const MONGODB_URI = process.env.MONGODB_URI || "mongodb://submarine:submar1ne@ds139768.mlab.com:39768/heroku_rx8cqg30";
-// const MONGODB_URI =
-//   process.env.MONGODB_URI ||
-//   "mongodb+srv://dbCaseyDean:Cdc108mng@cluster0-adjxe.mongodb.net/submarineDb?retryWrites=true&w=majority";
+const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://dbCaseyDean:Cdc108mng@cluster0-adjxe.mongodb.net/submarineDb?retryWrites=true&w=majority";
 
 // connect to MongoDB
 // moved database from mlag to cloud.mongodb.com. Had to create database in Cluster0 called submarine, and add collections users and subscriptions.
 
 mongoose.connect(
-  //   MONGODB_URI,
+    MONGODB_URI,
 //   process.env.MONGODB_URI ||
-    "mongodb+srv://dbCaseyDean:Cdc108mng@cluster0-adjxe.mongodb.net/submarine?retryWrites=true&w=majority",
+    // "mongodb+srv://dbCaseyDean:Cdc108mng@cluster0-adjxe.mongodb.net/submarine?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
     if (err) {
