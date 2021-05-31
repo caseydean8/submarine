3596e050bc5893ebba70f8bff5d6faba924b2b62
Casey Carroll <caseydean8@gmail.com>
2021-05-30 20:32:32 -0400

    Changed app.all to redirect "back"

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -95,7 +95,8 @@ module.exports = (app) => {
     });
   });
 
-  // app.all("*", function (req, res) {
-  //   res.redirect("https:///blooming-peak-13476.herokuapp.com/");
-  // });
+  app.all("*", function (req, res) {
+    // res.redirect("https:///blooming-peak-13476.herokuapp.com/");
+    res.redirect('back');
+  });
 };
